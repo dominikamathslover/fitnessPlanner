4edf85f67adf88fbbda290d5eea017af888829a0
Dominika <dominika.jasinska.lublin@gmail.com>
2025-08-19 23:30:01 +0200

    Add Get Mapping - endpoint username/workout

--- src/main/java/org/create/fitnessplenner/controller/WorkoutController.java
+++ src/main/java/org/create/fitnessplenner/controller/WorkoutController.java
@@ -1,4 +1,37 @@
 package org.create.fitnessplenner.controller;
 
+import org.create.fitnessplenner.model.User;
+import org.create.fitnessplenner.model.Workout;
+import org.create.fitnessplenner.repository.UserRepository;
+import org.create.fitnessplenner.repository.WorkoutRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Controller
 public class WorkoutController {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private WorkoutRepository workoutRepository;
+
+    @Transactional(readOnly = true)
+    @GetMapping("/{username}/workouts")
+    public String showUserWorkouts(@PathVariable String username, Model model) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+        List<Workout> workouts = workoutRepository.findWorkoutsWithTypeByUsername(username);
+        model.addAttribute("user", user);
+        model.addAttribute("workouts", workouts);
+        return "user-workouts";
+    }
+
 }
