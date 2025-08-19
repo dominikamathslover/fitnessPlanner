79e51a70d703ab94b025ede57028f2c19ffa299e
Dominika <dominika.jasinska.lublin@gmail.com>
2025-08-19 23:28:00 +0200

    Add repositories (user and workout)

--- src/main/java/org/create/fitnessplenner/repository/WorkoutRepository.java
+++ src/main/java/org/create/fitnessplenner/repository/WorkoutRepository.java
@@ -1,4 +1,17 @@
 package org.create.fitnessplenner.repository;
 
-public class WorkoutRepository {
+import org.create.fitnessplenner.model.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+
+    @Query("SELECT w FROM Workout w JOIN FETCH w.workoutType WHERE w.user.username = :username")
+    List<Workout> findWorkoutsWithTypeByUsername(@Param("username") String username);
+
 }
